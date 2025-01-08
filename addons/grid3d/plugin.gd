1c1e635c06d9b0c1ccf9f1746e4a6b4b0a5e861c
DevDrache <dominik.mast.11@gmail.com>
2025-01-08 06:52:28 +0200

    add agent node

--- addons/grid3d/plugin.gd
+++ addons/grid3d/plugin.gd
@@ -9,7 +9,14 @@ func _enter_tree() -> void:
 		preload("res://addons/grid3d/nodes/Grid3D/grid.gd"), 
 		preload("res://addons/grid3d/assets/Grid3D.png")
 	)
+	add_custom_type(
+		"GridAgent3D", 
+		"Node3D", 
+		preload("res://addons/grid3d/nodes/gridAgent3D/grid_agent_3d.gd"), 
+		preload("res://addons/grid3d/assets/Grid3D.png")
+	)
 
 
 func _exit_tree() -> void:
 	remove_custom_type("Grid3D")
+	remove_custom_type("GridAgent3D")
