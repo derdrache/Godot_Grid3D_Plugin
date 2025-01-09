187dda1f8318e7dae5a847f0d112c23d819505e9
DevDrache <dominik.mast.11@gmail.com>
2025-01-09 08:56:47 +0200

    rework cell script

--- addons/grid3d/nodes/Grid3D/gridCells/_grid_cell.gd
+++ addons/grid3d/nodes/Grid3D/gridCells/_grid_cell.gd
@@ -1,14 +1,13 @@
-@tool
 extends Area3D
-
-@onready var cell_mesh: MeshInstance3D = %CellMesh
-
+class_name GridCell3D
 
 @export var color: Color = Color.WHITE:
 	set(value):
 		if Engine.is_editor_hint():
 			color = value
 			_change_cell_color(value)
+			
+@onready var cell_mesh: MeshInstance3D = %CellMesh
 
 func _change_cell_color(newColor):
 	if not cell_mesh: return
@@ -29,23 +28,3 @@ func is_empty():
 	var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
 	var result: Dictionary = space_state.intersect_ray(query)
 	return result.is_empty()
-
-func set_size(size):
-	%CellMesh.mesh.size.x = size
-	%CellMesh.mesh.size.z = size
-	$CollisionShape3D.shape.size.x = size
-	$CollisionShape3D.shape.size.z = size
-	
-	_create_outline_mesh()
-
-func _create_outline_mesh():
-	%CellMesh.get_child(0).queue_free()
-	
-	var outlineMesh = MeshInstance3D.new()
-	outlineMesh.mesh = %CellMesh.mesh.create_outline(0.05 * %CellMesh.mesh.size.x)
-	
-	var material = StandardMaterial3D.new()
-	material.albedo_color = Color(0,0,0)
-	outlineMesh.set_surface_override_material(0, material)
-	
-	%CellMesh.add_child(outlineMesh)
---
+++ addons/grid3d/nodes/Grid3D/gridCells/_rect_grid_cell.gd
@@ -0,0 +1,22 @@
+@tool
+extends GridCell3D
+
+func set_size(size):
+	%CellMesh.mesh.size.x = size
+	%CellMesh.mesh.size.z = size
+	$CollisionShape3D.shape.size.x = size
+	$CollisionShape3D.shape.size.z = size
+	
+	_create_outline_mesh()
+
+func _create_outline_mesh():
+	%CellMesh.get_child(0).queue_free()
+	
+	var outlineMesh = MeshInstance3D.new()
+	outlineMesh.mesh = %CellMesh.mesh.create_outline(0.05 * %CellMesh.mesh.size.x)
+	
+	var material = StandardMaterial3D.new()
+	material.albedo_color = Color(0,0,0)
+	outlineMesh.set_surface_override_material(0, material)
+	
+	%CellMesh.add_child(outlineMesh)
