f576d09720d7b93726d8bcc8dda771f0ef9c1fdb
DevDrache <dominik.mast.11@gmail.com>
2025-01-09 08:56:28 +0200

    add circle cells

---
+++ addons/grid3d/nodes/Grid3D/gridCells/_circle_grid_cell.gd
@@ -0,0 +1,21 @@
+@tool
+extends GridCell3D
+
+func set_size(size):
+	%CellMesh.mesh.top_radius = size / 2.0
+	%CellMesh.mesh.bottom_radius = size / 2.0
+	$CollisionShape3D.shape.radius = size / 2.0
+	
+	_create_outline_mesh()
+
+func _create_outline_mesh():
+	%CellMesh.get_child(0).queue_free()
+	
+	var outlineMesh = MeshInstance3D.new()
+	outlineMesh.mesh = %CellMesh.mesh.create_outline(0.05 * %CellMesh.mesh.top_radius)
+	
+	var material = StandardMaterial3D.new()
+	material.albedo_color = Color(0,0,0)
+	outlineMesh.set_surface_override_material(0, material)
+	
+	%CellMesh.add_child(outlineMesh)
--- addons/grid3d/nodes/grid3D/grid.gd
+++ addons/grid3d/nodes/grid3D/grid.gd
@@ -2,11 +2,6 @@
 extends Node3D
 class_name Grid3D
 
-@export var refresh := false:
-	set(value):
-		if Engine.is_editor_hint() and is_inside_tree():
-			_refresh_grid()
-		
 @export var gridSize := 5:
 	set(value): 
 		gridSize = value
@@ -24,12 +19,28 @@ class_name Grid3D
 		margin = value
 		if Engine.is_editor_hint() and is_inside_tree():
 			_refresh_grid()
-		
-@export var cellNode := preload("res://addons/grid3d/nodes/Grid3D/_grid_cell.tscn")
+			
+@export var cellType : Cell_Types:
+	set(value):
+		cellType = value
+		if Engine.is_editor_hint() and is_inside_tree():
+			_refresh_grid()
+
+@export var debug_refresh := false:
+	set(value):
+		if Engine.is_editor_hint() and is_inside_tree():
+			_refresh_grid()
+
+enum Cell_Types{RECT, CIRCLE}
+
+const _RECT_GRID_CELL = preload("res://addons/grid3d/nodes/Grid3D/gridCells/_rect_grid_cell.tscn")
+const _CIRCLE_GRID_CELL = preload("res://addons/grid3d/nodes/Grid3D/gridCells/_circle_grid_cell.tscn")
 
 func _ready() -> void:
 	add_to_group("Grid3D")
-
+	
+	_remove_grid()
+	
 	_refresh_grid()
 
 func _refresh_grid():
@@ -44,7 +55,7 @@ func _remove_grid():
 func _generate_grid():
 	for z in range(gridSize):
 		for x in range(gridSize):
-			var grid_cell_instance = cellNode.instantiate()
+			var grid_cell_instance = _get_grid_cell_node()
 			grid_cell_instance.set_size(cellSize)
 			
 			add_child(grid_cell_instance)
@@ -53,6 +64,11 @@ func _generate_grid():
 				grid_cell_instance.owner = get_tree().edited_scene_root
 			grid_cell_instance.name = str(z + 1) +  "," + str(x+1)
 			grid_cell_instance.global_position = global_position + Vector3(x * (cellSize + margin),0.1, z * (cellSize + margin))
+
+func _get_grid_cell_node():
+	match cellType:
+		Cell_Types.RECT: return _RECT_GRID_CELL.instantiate()
+		Cell_Types.CIRCLE: return _CIRCLE_GRID_CELL.instantiate()
 
 func get_cell_space():
 	return cellSize + margin
