273de118ce1eaa5bae694d5b60623d89a2154a9d
DevDrache <dominik.mast.11@gmail.com>
2026-09-03 08:39:54 +0300

    add circle grid

--- addons/grid3d/nodes/Grid3D/grid_shader.gd
+++ addons/grid3d/nodes/Grid3D/grid_shader.gd
@@ -2,6 +2,11 @@
 extends StaticBody3D
 class_name Grid3D_New
 
+@export var gridType:GRID_TYPES = GRID_TYPES.RECTANGLE:
+	set(value):
+		gridType = value
+		_change_grid_type()
+		notify_property_list_changed()
 @export var gridSize := Vector2(10,10):
 	set(value):
 		gridSize = value
@@ -10,11 +15,15 @@ class_name Grid3D_New
 	set(value):
 		cellSize = value
 		_refresh_grid() 
+
 @export var borderWidth := 0.01:
 	set(value):
 		borderWidth = value
 		$GridMesh.material_override.set_shader_parameter("borderWidth", borderWidth)
-
+@export var circleRadius := 0.5:
+	set(value):
+		circleRadius = value
+		$GridMesh.material_override.set_shader_parameter("circleRadius", circleRadius)
 @export var cellColor := Color.BLACK:
 	set(value):
 		cellColor = value
@@ -29,11 +38,23 @@ class_name Grid3D_New
 @onready var grid_mesh: MeshInstance3D = $GridMesh
 @onready var grid_highligh_node: Node3D = $GridHighlighNode
 
+enum GRID_TYPES {RECTANGLE, CIRCLE}
+
+const RECTANGLE_GRID = preload("uid://cigab7eamjibn")
+const CIRCLE_GRID = preload("uid://dpvc02h58ghi2")
+
 var highlightedCells: Dictionary[String, Array] = {
 		"positions": [],
 		"colors": []
 	}
-		
+
+func _change_grid_type():
+	var shader: Resource
+	match gridType:
+		GRID_TYPES.RECTANGLE: shader = RECTANGLE_GRID
+		GRID_TYPES.CIRCLE: shader = CIRCLE_GRID
+	
+	$GridMesh.get_active_material(0).shader = shader
 
 func _ready() -> void:
 	add_to_group("Grid3D")
@@ -96,3 +117,15 @@ func reset_highlight():
 	highlightedCells.colors = []
 	
 	_refresh_grid_highlight_node()
+
+func _validate_property(property: Dictionary) -> void:
+	var hideList = []
+	
+	match gridType:
+		GRID_TYPES.RECTANGLE:
+			hideList.append("circleRadius")
+		GRID_TYPES.CIRCLE:
+			hideList.append("borderWidth")
+		
+	if property.name in hideList: 
+		property.usage = PROPERTY_USAGE_NO_EDITOR
